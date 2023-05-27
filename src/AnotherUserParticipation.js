78449bb63309dd464ebd5f483d91a928c590a325
Kashish <kashishkirplani618@gmail.com>
2023-05-27 17:57:34 +0500

    Add AnotherUserParticipation Component

---
+++ src/AnotherUserParticipation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AnotherUserParticipation(props) {
+    return (
+        <div>
+            <button>Reset Selection Participation</button>
+        </div>
+    );
+}
+
+export default AnotherUserParticipation;
