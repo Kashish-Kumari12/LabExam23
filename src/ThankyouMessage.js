af036b37c472c73912b4e6640fd8cb774d711cdc
Kashish <kashishkirplani618@gmail.com>
2023-05-27 17:55:14 +0500

    Add ThankyouMessage Component

---
+++ src/ThankyouMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ThankyouMessage(props) {
+    return (
+        <div>
+           Thankyou for voting! 
+        </div>
+    );
+}
+
+export default ThankyouMessage;
